ccc80ae0d5db41c08989b1502c7f68e23a5bc27d
david63011 <david630@gmail.com>
2022-09-16 11:19:46 -0300

    Add chanage

--- main.js
+++ main.js
@@ -220,23 +220,10 @@ formSection.addEventListener('submit', (e) => {
   }
 });
 
-const storageForm = {
-  name: '',
+const myObj = {
+  name: 'brad',
   email: '',
   message: '',
 };
-
-formSection.addEventListener('save', () => {
-  storageForm.name = document.getElementById('full-name').value;
-  storageForm.email = document.getElementById('email').value;
-  storageForm.message = document.getElementById('text-box').value;
-  localStorage.setItem('myStore', JSON.stringify(storageForm));
-});
-
-const storObj = JSON.parse(localStorage.getItem('myStore'));
-
-document.getElementById('full-name').value = storObj.name;
-document.getElementById('email').value = storObj.email;
-document.getElementById('text-box').value = storObj.message;
-
-
+const myObj1 = JSON.stringify(myObj)
+localStorage.setItem('myObj', myObj1)
